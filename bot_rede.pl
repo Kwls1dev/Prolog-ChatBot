6b1ffc2683e1c5e85550c575fc2823ed68c69bdd
Kaique Wilson <91637147+Kwls1dev@users.noreply.github.com>
2023-04-27 12:58:53 -0300

    Add files via upload

---
+++ bot_rede.pl
@@ -0,0 +1,29 @@
+% Projeto Integrador Em Ciência da Computação
+% Autor: Kaique Wilson Lizardo da Silva.
+% Projeto: ChatBot de Perguntas e Respostas.
+% Assunto: Redes de Computadores.
+% Linguagem: Prolog.
+
+
+:-include(base_dados1_rede).
+:-include(base_dados2_rede).
+		
+bot_rede:-
+	format('Saudações! Eu sou o Bot_Rede.'),nl,
+	format('Voce deseja saber sobre: '),nl,
+	  				
+	repeat,
+	nl, format('- '),
+	read(Entrada),
+	consulta_base_dados(Entrada, Resposta),
+	format(Resposta), nl,
+	encerra_sessao(Entrada).
+	
+consulta_base_dados(Entrada, RespostaCorreta) :-
+	definida_por(Entrada, RespostaCorreta), !.	
+	
+encerra_sessao(Entrada):-
+	Entrada = ('fim').
+	
+	
+ 
